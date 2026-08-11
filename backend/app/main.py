1b9632282f0ed2b28ecbfa0cb9cd51377b6f3249
Jayaprakash <chitrakalam15@gmail.com>
2026-08-11 08:17:18 +0530

    chore: optimize cors parameters to eliminate all production connection filters

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,28 +1,25 @@
 from fastapi import FastAPI
-from .database import Base, engine
-from .models import Customer
-from .routers import customers,payments
 from fastapi.middleware.cors import CORSMiddleware
-
-
+from .database import engine, Base
+from .routers import customers, payments
 
 Base.metadata.create_all(bind=engine)
 
-app=FastAPI(title="Billing Engine")
+app = FastAPI(title="Billing Engine")
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],             
+    allow_credentials=False,        
+    allow_methods=["*"],            
+    allow_headers=["*"],             
+)
+
 
 app.include_router(customers.router)
 app.include_router(payments.router)
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-    allow_origins=["*"],
-)
-
-
 @app.get("/")
 def root():
     return {"message": "Billing Engine API is running"}
-
